7a05ccb47d7582a2714fc2d970aeecf58dd2ed4e
Sagit Khaliullin <sgtkhaliullin@gmail.com>
2018-05-02 13:01:41 +0300

    Negative/Positive recognition added

--- parser.py
+++ parser.py
@@ -7,10 +7,12 @@ def read_csv(filename):
     """
     Возвращает словарь {'title':, 'text':}
     """
+    is_positive = filename[-5] == '1'
     reader = csv.reader(open(filename, newline='\n'), delimiter='\n')
     review_list = list(reader)
     review_dict = {'title': review_list[0][0],
-                   'text': ' '.join(str(review) for paragraph in review_list[1:] for review in paragraph)}
+                   'text': ' '.join(str(review) for paragraph in review_list[1:] for review in paragraph),
+                   'positive': is_positive}
     return review_dict
 
 
@@ -21,22 +23,21 @@ def read_corpus(corpus, direction_name='', limit=-2):
     :param limit: Количество читаемых файлов из корпуса
     :return: Пока ничего не возвращает
     """
+    list_dicts = []
     if direction_name:
         folder = os.path.join(corpus, direction_name)
         for review in os.listdir(folder)[:limit]:
-            # print(review)
-            r = read_csv(os.path.join(corpus, direction_name, review))
-            print(r)
+            list_dicts.append(read_csv(os.path.join(corpus, direction_name, review)))
     else:
         folder = os.listdir(corpus)
         for direction in folder:
             if not direction.startswith("."):
                 for review in os.listdir(os.path.join(corpus, direction))[:limit]:
-                    # print(review)
-                    r = read_csv(os.path.join(corpus, direction, review))
-                    print(r)
+                    list_dicts.append(read_csv(os.path.join(corpus, direction, review)))
+    return list_dicts
 
 
 if __name__ == '__main__':
     corpus = 'corpus'
-    read_corpus(corpus, direction_name='Аптеки', limit=40)
+    l = read_corpus(corpus, direction_name='Аптеки', limit=40)
+    print(l)
