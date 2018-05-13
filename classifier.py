b5057019fcba9fdcb3761ea42ef38740fdcd035d
Marsel Mustafin <mustafinkzn97@gmail.com>
2018-05-13 14:33:21 +0300

    Simplify classifier

--- classifier.py
+++ classifier.py
@@ -1,76 +1,45 @@
-import lightgbm as lgb
+from lightgbm import LGBMClassifier
 import numpy as np
+from nltk import RegexpTokenizer
 from sklearn import svm
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, FeatureUnion
 
 from parser import Parser
 
 
 class Classifier:
 
-    @staticmethod
-    def get_bayes_result(docs):
-        twenty_train, twenty_test, target_train, target_test = train_test_split(docs['text'], docs['target'],
-                                                                                test_size=0.2,
-                                                                                random_state=len(docs))
-        text_clf = Pipeline([('vect', CountVectorizer()),
-                             ('tfidf', TfidfTransformer()),
-                             ('clf', MultinomialNB()),
-                             ])
-        text_clf.fit(twenty_train, target_train)
+    def get_bayes_result(self, docs):
+        return self.run_pipeline_with_clf(MultinomialNB(), docs)
 
-        predicted = text_clf.predict(twenty_test)
-        return np.mean(predicted == target_test)
+    def get_svm_result(self, docs):
+        return self.run_pipeline_with_clf(svm.SVC(), docs)
 
-    @staticmethod
-    def get_svm_result(docs):
+    def get_regression_result(self, docs):
+        return self.run_pipeline_with_clf(LogisticRegression(), docs)
+
+    def get_lgbm_result(self, docs):
+        return self.run_pipeline_with_clf(LGBMClassifier(), docs)
+
+    def run_pipeline_with_clf(self, clf, docs):
         twenty_train, twenty_test, target_train, target_test = train_test_split(docs['text'], docs['target'],
-                                                                                test_size=0.2,
+                                                                                test_size=0.3,
                                                                                 random_state=1)
         text_clf = Pipeline([('vect', CountVectorizer()),
                              ('tfidf', TfidfTransformer()),
-                             ('clf', svm.SVC()),
+                             ('clf', clf),
                              ])
         text_clf.fit(twenty_train, target_train)
 
         predicted = text_clf.predict(twenty_test)
         return np.mean(predicted == target_test)
 
-    @staticmethod
-    def get_regression_result(docs):
-        twenty_train, twenty_test, target_train, target_test = train_test_split(docs['text'], docs['target'],
-                                                                                test_size=0.2,
-                                                                                random_state=len(docs))
-        text_clf = Pipeline([('vect', CountVectorizer()),
-                             ('tfidf', TfidfTransformer()),
-                             ('clf', LogisticRegression()),
-                             ])
-        text_clf.fit(twenty_train, target_train)
-
-        predicted = text_clf.predict(twenty_test)
-        return np.mean(predicted == target_test)
-
-    @staticmethod
-    def get_lgbm_result(docs):
-        twenty_train, twenty_test, target_train, target_test = train_test_split(docs['text'], docs['target'],
-                                                                                test_size=0.2,
-                                                                                random_state=len(docs))
-        text_clf = Pipeline([('vect', CountVectorizer()),
-                             ('tfidf', TfidfTransformer()),
-                             ('clf', lgb.sklearn.LGBMClassifier()),
-                             ])
-        text_clf.fit(twenty_train, target_train)
-
-        predicted = text_clf.predict(twenty_test)
-        return np.mean(predicted == target_test)
-
-    @staticmethod
-    def get_data(docs):
+    def set_data(self, docs):
         result = dict()
         text = []
         target = []
@@ -86,7 +55,7 @@ if __name__ == '__main__':
     pr = Parser()
     classifier = Classifier()
     docs = pr.read_csv('new_corpus/Аптеки.csv', 1000)
-    data = classifier.get_data(docs)
+    data = classifier.set_data(docs)
     print("Logistic Regression", classifier.get_regression_result(data))
     print("SVM", classifier.get_svm_result(data))
     print("Naive Bayes", classifier.get_bayes_result(data))
